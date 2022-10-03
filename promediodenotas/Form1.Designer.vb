5c37de2dfe4f98f61c67b0b3242383ae991bcd11
yair arroyo <optimusforxty123@gmail.com>
2022-10-03 16:39:38 -0500

    promedio de nota estudiantil

---
+++ promediodenotas/Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtnota1 = New System.Windows.Forms.TextBox()
+        Me.txtnota2 = New System.Windows.Forms.TextBox()
+        Me.txtnota3 = New System.Windows.Forms.TextBox()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.btnlimpiar = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtcondicion = New System.Windows.Forms.TextBox()
+        Me.txtpromedio = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese Nota 1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ingrese Nota 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Ingrese Nota 3:"
+        '
+        'txtnota1
+        '
+        Me.txtnota1.Location = New System.Drawing.Point(137, 23)
+        Me.txtnota1.Name = "txtnota1"
+        Me.txtnota1.Size = New System.Drawing.Size(100, 20)
+        Me.txtnota1.TabIndex = 3
+        '
+        'txtnota2
+        '
+        Me.txtnota2.Location = New System.Drawing.Point(137, 57)
+        Me.txtnota2.Name = "txtnota2"
+        Me.txtnota2.Size = New System.Drawing.Size(100, 20)
+        Me.txtnota2.TabIndex = 4
+        '
+        'txtnota3
+        '
+        Me.txtnota3.Location = New System.Drawing.Point(137, 89)
+        Me.txtnota3.Name = "txtnota3"
+        Me.txtnota3.Size = New System.Drawing.Size(100, 20)
+        Me.txtnota3.TabIndex = 5
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Location = New System.Drawing.Point(26, 138)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btncalcular.TabIndex = 6
+        Me.btncalcular.Text = "CALCULAR"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'btnlimpiar
+        '
+        Me.btnlimpiar.Location = New System.Drawing.Point(137, 138)
+        Me.btnlimpiar.Name = "btnlimpiar"
+        Me.btnlimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnlimpiar.TabIndex = 7
+        Me.btnlimpiar.Text = "LIMPIAR"
+        Me.btnlimpiar.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 78)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(112, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Condicion del Alumno:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(23, 40)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Promedio"
+        '
+        'txtcondicion
+        '
+        Me.txtcondicion.Location = New System.Drawing.Point(143, 75)
+        Me.txtcondicion.Name = "txtcondicion"
+        Me.txtcondicion.Size = New System.Drawing.Size(155, 20)
+        Me.txtcondicion.TabIndex = 10
+        '
+        'txtpromedio
+        '
+        Me.txtpromedio.Location = New System.Drawing.Point(143, 37)
+        Me.txtpromedio.Name = "txtpromedio"
+        Me.txtpromedio.Size = New System.Drawing.Size(100, 20)
+        Me.txtpromedio.TabIndex = 11
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtnota1)
+        Me.GroupBox1.Controls.Add(Me.txtnota2)
+        Me.GroupBox1.Controls.Add(Me.btnlimpiar)
+        Me.GroupBox1.Controls.Add(Me.txtnota3)
+        Me.GroupBox1.Controls.Add(Me.btncalcular)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(304, 180)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "NOTAS DEL ALUMNO"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.txtpromedio)
+        Me.GroupBox2.Controls.Add(Me.txtcondicion)
+        Me.GroupBox2.Location = New System.Drawing.Point(42, 225)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(304, 124)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "RESULTADO DEL ALUMNO"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 351)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtnota1 As TextBox
+    Friend WithEvents txtnota2 As TextBox
+    Friend WithEvents txtnota3 As TextBox
+    Friend WithEvents btncalcular As Button
+    Friend WithEvents btnlimpiar As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtcondicion As TextBox
+    Friend WithEvents txtpromedio As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ promediodenotas/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
+        Dim nota1, nota2, nota3, promedio As Double
+        Dim condicion As String
+
+        nota1 = txtnota1.Text
+        nota2 = txtnota2.Text
+        nota3 = txtnota3.Text
+
+        promedio = (nota1 + nota2 + nota3) / 3
+
+        If promedio <= 13 Then
+            condicion = "DESPROBADO"
+            txtcondicion.Text = condicion
+            txtpromedio.Text = promedio
+
+        ElseIf promedio >= 14 And promedio <= 20 Then
+            condicion = "APROBADO"
+            txtcondicion.Text = condicion
+            txtpromedio.Text = promedio
+
+        Else
+            condicion = "NOTA FUERA DE RANGO"
+            txtcondicion.Text = condicion
+            txtpromedio.Text = promedio
+
+        End If
+
+    End Sub
+
+    Private Sub btnlimpiar_Click(sender As Object, e As EventArgs) Handles btnlimpiar.Click
+        txtnota1.Clear()
+        txtnota2.Clear()
+        txtnota3.Clear()
+        txtcondicion.Clear()
+        txtpromedio.Clear()
+
+    End Sub
+End Class
